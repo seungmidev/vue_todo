31c5647d32e29d65d26c9b0af6fd496b6c7a5c4b
seungmidev <seungmilee.dev@gmail.com>
2024-02-27 13:07:22 +0900

    Add vue todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="text-center">ToDo App</h1>
+    <input type="text" class="w-100 p-2" placeholder="New ToDos" @keyup.enter="addTodo" v-model="todoText">
+    <hr>
+    <to-do-item 
+      v-for="todo in todos" 
+      :key="todo.id" 
+      :todo="todo" 
+      @toogle-checkbox="toggleCheckbox"
+      @click-delete="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { 
+    ToDoItem 
+  },
+  data() {
+    return {
+      todoText: '',
+      todos: [
+        { id: 1, text: 'Add a todo to get started', checked: false },
+      ]
+    }
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+      });
+      this.todoText = '';
+    },
+    toggleCheckbox({ id, checked }) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos[index].checked = checked;
+    },
+    deleteTodo(id) {
+      // const index = this.todos.findIndex(todo => {
+      //   return todo.id === id;
+      // });
+      // this.todos.splice(index, 1); 
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
